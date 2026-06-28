ca59800db83d685d2d116bdbe6f7b0b6d887269b
Anonymzx <qhidalrapz@gmail.com>
2026-06-29 03:59:42 +0700

    git commit -m "Release 1.3.7"

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ import logging
 import os as _os
 import sys as _sys
 
-__version__ = "1.3.6"
+__version__ = "1.3.7"
 __author__ = "Anonymzx"
 
 # Ensure this package is on sys.path for absolute imports to work
